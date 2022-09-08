8aad7f9c2c7ab536baee2065f7a0fc4207d458f9
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-09-08 10:44:33 -0500

    googleDrive iFrame setup successful

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,13 @@ import PropTypes from 'prop-types';
 function Home() {
   return (
     <div>
-      This is home page
+      This is home page<br/>
+      <iframe
+        frameBorder="0"
+        width="500"
+        height="60"
+        src="https://drive.google.com/file/d/1oOj5RIgQh1jT_D25Cvjnm_F8r55lTeSP/preview">
+      </iframe>
     </div>
   );
 }
