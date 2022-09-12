d50d1eb997949aeffb59cdede442e78d68a13f8d
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-09-11 19:17:20 -0500

    removed jest option from eslintrc

--- src/App/index.js
+++ src/App/index.js
@@ -1,6 +1,6 @@
 // import firebase from 'firebase/app';
 import 'firebase/auth';
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from '../components/NavBar';
 // import firebaseConfig from '../helpers/apiKeys';
 import Routes from '../helpers/Routes';
