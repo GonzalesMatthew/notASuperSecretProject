1d086f9c0353b29eaf457750791ff29e1eb9ddec
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-09-18 15:34:44 -0500

    successfully converted src/index.js to .tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <React.Fragment>
     <App />
-  </React.Fragment>,
-  document.getElementById('root')
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
