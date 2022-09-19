dea7a47a2bc5d3190ddecc91757d3606d4ee448b
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-09-19 08:36:46 -0500

    added issuel/resolver to settings in eslintrc and added .tsx to tsx files in imports

--- src/App/index.js
+++ src/App/index.js
@@ -1,7 +1,7 @@
 // import firebase from 'firebase/app';
 import 'firebase/auth';
 import React, { useState, useEffect } from 'react';
-import NavBar from '../components/NavBar';
+import NavBar from '../components/NavBar.tsx';
 // import firebaseConfig from '../helpers/apiKeys';
 import Routes from '../helpers/Routes';
 import './App.scss';
