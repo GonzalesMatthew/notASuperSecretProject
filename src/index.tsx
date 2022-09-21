2127b291092a5b4213441db210b3ba0329830ca9
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-09-20 19:36:01 -0500

    updated v9 firebase. removed reportWebVitals: turned off

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createRoot } from 'react-dom/client';
 import './styles/index.scss';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import reportHandler from './reportHandler';
+// import reportWebVitals from './reportWebVitals';
+// import reportHandler from './reportHandler';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
@@ -17,4 +16,5 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(reportHandler);
+
+// reportWebVitals(reportHandler);
