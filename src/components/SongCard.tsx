791c9d765d1dcbe914ceb0e2ae456b007e4b8bb3
Matthew Gonzales <gonzalesmattg@gmail.com>
2022-09-25 14:14:02 -0500

    made card for songs, troubleshooting typescript syntax

---
+++ src/components/SongCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const SongCard = ({ title, path }) => {
+  <>
+    <div>
+      {title}<br/>
+      <iframe
+        frameBorder="0"
+        width="500"
+        height="250"
+        src={path}
+      ></iframe>
+    </div>
+  </>
+};
+
+SongCard.propTypes = {
+  title: PropTypes.string,
+  path: PropTypes.string
+};
+
+export default SongCard;
