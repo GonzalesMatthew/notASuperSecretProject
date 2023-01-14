572e2915a411a364ae136f044a7918360271b764
Matthew Gonzales <gonzalesmattg@gmail.com>
2023-01-14 15:38:53 -0800

    getting closer to desired navigation - keeping music going while hiding other components. would like to do so with music comp without rerendering it

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,5 +1,6 @@
 import 'firebase/auth';
 import React, { useState } from 'react';
+import Music from '../components/Music';
 import NavBar from '../components/NavBar';
 import Routes from '../helpers/Routes';
 import './App.scss';
@@ -10,6 +11,9 @@ function App() {
   return (
     <>
       <NavBar setToggle={setToggle}/>
+      <div id='musicDiv'>
+        <Music/>
+      </div>
       <Routes toggle={toggle}/>
     </>
   );
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -8,43 +8,34 @@ const Home = ({ toggle }: InferProps<typeof Home.propTypes>) => {
   console.warn('Home component mounted');
 
   useEffect(() => {
-    const musicDiv = document.getElementById('musicComp') as HTMLElement;
     const releasesDiv = document.getElementById('releasesComp') as HTMLElement;
     const contactDiv = document.getElementById('contactComp') as HTMLElement;
     switch (toggle) {
       case 'Music':
-        musicDiv.style.display = 'block';
         releasesDiv.style.display = 'none';
         contactDiv.style.display = 'none';
         break;
       case 'Releases':
-        musicDiv.style.display = 'none';
         releasesDiv.style.display = 'block';
         contactDiv.style.display = 'none';
         break;
       case 'Contact':
-        musicDiv.style.display = 'none';
         releasesDiv.style.display = 'none';
         contactDiv.style.display = 'block';
         break;
       default:
         console.warn('switch code error');
     }
-  }, [toggle]);
+  }, []);
 
   return (
     <>
-      <div id="musicComp">
-        <Music/>
-      </div>
       <div id="releasesComp">
         <Releases/>
       </div>
       <div id="contactComp">
         <Contact/>
       </div>
-      {/* <button type='button' onClick={() => hide()}> Hide Element </button>
-        <button type='button' onClick={() => show()}> Show Element </button> */}
     </>
   );
 };
